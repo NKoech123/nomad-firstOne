42d4540f778e7a1e7531b2e70054b6176d2b8439
Nicholas Koech <nicholaskoech@Nicholass-MacBook-Pro.local>
2022-11-05 22:11:42 -0700

    added businessCategory table

--- backend/nomad/models.py
+++ backend/nomad/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 class Vendor(models.Model):
     name = models.CharField(max_length=200)
+    owner_name = models.CharField(max_length=200)
+    geolocation = models.CharField(max_length=200)
+    businessCategoryID = models.ForeignKey('BusinessCategory', on_delete=models.CASCADE,null=False)
+    global_score = models.IntegerField()
+    friends_score = models.IntegerField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
 
+class BusinessCategory(models.Model):
+    name = models.CharField(max_length=200)
